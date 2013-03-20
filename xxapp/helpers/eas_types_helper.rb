4376e0d047d12db4bf48674e4490712876b32710
Arelem <arelem3@bigpond.com>
2013-03-20 14:07:59 +1100

    Change the apps folder with stylesheets

--- xxapp/helpers/eas_types_helper.rb
+++
@@ -1,2 +0,0 @@
-module EasTypesHelper
-end
--- xxapp/models/attendee_eas_types.rb
+++
@@ -1,8 +0,0 @@
-class AttendeeEasTypes < ActiveRecord::Base
-  attr_accessible :attendee_id, :eas_types_id
-  
-  belongs_to :attendee
-  belongs_to :payment
-  belongs_to :eas_type
-
-end
