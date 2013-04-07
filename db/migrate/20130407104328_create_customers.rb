ca2560fcdb12a341ecc8de84b3655e5379b92f39
Arelem <arelem3@bigpond.com>
2013-04-07 21:34:04 +1000

    Error in the Customer migration - see heroku logs for details Apr7,9:35pm

--- db/migrate/20130407104328_create_customers.rb
+++ db/migrate/20130407104328_create_customers.rb
@@ -1,17 +1,17 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-      t.integer :age
-      t.string :gender
-      t.string :fname
-      t.string :lname
-      t.string :eck_id
-      t.string :address
-      t.string :state
-      t.string :country
-      t.string :contact
-      t.string :designation
-      t.integer :donation
+      t.integer :age, :null =>false
+      t.string :gender, :null =>false
+      t.string :fname, :null =>false
+      t.string :lname, :null =>false
+      t.string :eck_id, :null =>false
+      t.string :address, :null =>false
+      t.string :state, :null =>false
+      t.string :country, :null =>false
+      t.string :contact, :null =>false
+      t.string :designation, :null =>false
+      t.integer :donation, :null =>false
 
       t.timestamps
     end
