27654733c585cd52f7a6082cde674faf8dfea863
Arelem <arelem3@bigpond.com>
2013-04-07 22:01:22 +1000

    CaseSensitiveValidator Error, probably line 5 in customer Apr7,10:00pm

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,13 +2,13 @@ class Customer < ActiveRecord::Base
   attr_accessible :address, :age, :contact, :country, :designation, :donation, :eck_id, :fname, :gender, :lname, :state
 
   validates :age, :inclusion => { :in => 0..99 }
-  validates :gender, :inclusion => %w(male female), :case_sensitive => false
+  validates :gender, :inclusion => %w(male female)
   validates :address, :presence => true, :length => { :maximum => 55 }
   validates :contact, :presence => true, :length => { :maximum => 16 }
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
   validates :designation, :presence => true, :length => { :maximum => 15 }
-  validates :donation, :presence => true, :numericality => {:greater_than => -1}
-  validates :eck_id, :presence => true, :uniqueness => {:case_sensitive => false}
+  validates :donation, :presence => true, :numericality => {:greater_than => 0}
+  validates :eck_id, :presence => true, :uniqueness => true
   validates :fname, :presence => true, :length => { :maximum => 15 }, :exclusion => { :in => %w(admin superuser) }
   validates :lname, :presence => true, :length => { :maximum => 15 }, :exclusion => { :in => %w(admin superuser) }
   validates :state, :presence => true, :length => { :maximum => 3 }, :inclusion => %w(QLD NSW ACT VIC TAS SA WA NT), :case_sensitive => true
