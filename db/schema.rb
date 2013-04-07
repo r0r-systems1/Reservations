f2d7d2496a4dfcf8205f4e65240d760573d4da12
Arelem <arelem3@bigpond.com>
2013-04-07 21:00:09 +1000

    Removed Users table Apr7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407104747) do
+ActiveRecord::Schema.define(:version => 20130407105804) do
 
   create_table "attendee_eas_types", :force => true do |t|
     t.integer  "eas_types_id", :null => false
@@ -116,14 +116,6 @@ ActiveRecord::Schema.define(:version => 20130407104747) do
     t.integer  "youth_price"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "hashed_password"
-    t.string   "salt"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
   end
 
 end
